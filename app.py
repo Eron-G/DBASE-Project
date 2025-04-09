2f0270b10d3166396d346b33fed26a551f4d0cb1
Eron-G <161703565+Eron-G@users.noreply.github.com>
2025-04-10 02:34:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import mysql.connector
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 from werkzeug.middleware.proxy_fix import ProxyFix
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 from datetime import datetime, timedelta
 from functools import wraps
 
